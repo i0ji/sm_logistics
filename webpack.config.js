3e31bf9c90aa2fb0bd077d7e4dcb73edafe1d5a5
HardTurn <ioji@inbox.ru>
2022-12-29 13:33:40 +0300

    bootstrap learning

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const miniCss = require('mini-css-extract-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -11,7 +12,16 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
     },
     plugins: [
         new HTMLWebpackPlugin({
@@ -19,10 +29,13 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                { from: "img", to: "img" },
+                {from: "img", to: "img"},
             ],
         }),
         new CleanWebpackPlugin(),
+        new miniCss({
+            filename: 'style.css'
+        })
     ],
     devServer: {
         static: {
